186dfe454834c6ca2faec9b8ab4ea8e07bb3d90a
maadesh <151980884+Maadesh11@users.noreply.github.com>
2024-06-10 12:15:35 +0530

    ifelseifloop.js

---
+++ ifelseifloop.js
@@ -0,0 +1,10 @@
+let num=-2;
+if(num>10){
+    document.write("given num is big");
+}
+else if(num<10){
+    document.write("given num is lesser");
+}
+else{
+    document.write("given num is 10");
+}
